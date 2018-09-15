abb10b0843e8c1c37a282b3dc1f9f6dbed893dba
Akash Kundu <akashkundu92@gmail.com>
2018-09-15 06:19:16 -0500

    created reading view component

--- reading-adventures/src/app/reading-view/reading-view.component.ts
+++ reading-adventures/src/app/reading-view/reading-view.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reading-view',
   templateUrl: './reading-view.component.html',
-  styleUrls: ['./reading-view.component.sass']
+  styleUrls: ['./reading-view.component.css']
 })
 export class ReadingViewComponent implements OnInit {
 

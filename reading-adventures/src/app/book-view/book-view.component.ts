0d0bd96b65beea94d1d95e3acd5b1523d6221404
Akash Kundu <akashkundu92@gmail.com>
2018-09-15 04:41:06 -0500

    developing basic layout of book view

--- reading-adventures/src/app/book-view/book-view.component.ts
+++ reading-adventures/src/app/book-view/book-view.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-book-view',
   templateUrl: './book-view.component.html',
-  styleUrls: ['./book-view.component.sass']
+  styleUrls: ['./book-view.component.css']
 })
 export class BookViewComponent implements OnInit {
 

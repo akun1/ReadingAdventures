36e7ce5ba8e74907511fbd516a51761f8cced5f1
Akash Kundu <akashkundu92@gmail.com>
2018-09-18 14:12:38 -0500

    started to create subject for get bible service

--- reading-adventures/src/app/get-bible.service.ts
+++ reading-adventures/src/app/get-bible.service.ts
@@ -10,14 +10,12 @@ import { Subject } from 'rxjs';
 export class GetBibleService {
 
   private base_api_url : string = "http://getbible.net/json?";
-  catalog : Book[] = [];
-  public books$ : Subject<Book> = new Subject();
+  bible_text;
+  public books$ = new Subject();
   
   constructor(private http: HttpClient) {
-    this.http.get<Book[]>(this.base_api_url + 'p=Jn3:1-35').subscribe((res) => {
-      res.forEach(element => {
-        this.books$.next(new Book(element["author"],element["id"],element["thumbnail_url"],element["summary"],element["Title"]));
-      });
+    this.http.get(this.base_api_url + 'p=Jn3:1-35').subscribe((res) => {
+      this.bible_text = res;
     });
   }
 }

51508553534fee5bb4d70643f080b2b7b87ab4c1
Akash Kundu <akashkundu92@gmail.com>
2018-10-27 10:41:22 -0500

    added first two book entries

--- reading-adventures/src/app/reading-view/reading-view.component.ts
+++ reading-adventures/src/app/reading-view/reading-view.component.ts
@@ -21,10 +21,19 @@ export class ReadingViewComponent implements OnInit {
   section_name;
   chapters;
   hidden : boolean = true;
+  page1;
+  page2;
+  all_chapter_names;
+  currentBookName = "Genesis";
 
   constructor(private route: ActivatedRoute, private router: Router, private _bibleService: GetBibleService) {}
 
   ngOnInit() {
+
+    this.page1 = { book: "Genesis", chapter: 1 };
+    this.page2 = { book: "Mark", chapter: 1 };
+
+
     this.id = this.route.snapshot.paramMap.get('id');
     var result = this.getBookWithId(this.id);
     if(result.title.length > 0) {
@@ -97,32 +106,52 @@ export class ReadingViewComponent implements OnInit {
     }
   }
 
-  previousPage() {
-    if(this.book_entry !== null) {
-      if(this.currentBook.title == "Bible") {
-        if(this.pageNumber < 2) {
-          alert('This is the first page of '+ this.currentBook.title +'!');
-        }
-        else {
-          this.pageNumber--;
-          this.displayPage();
-        }
-      }
+  switchText() {
+    var element = document.getElementById("entire-genesis");
+    element.classList.toggle("text-hidden");
+    var element = document.getElementById("entire-mark");
+    element.classList.toggle("text-hidden");
+    if(this.currentBookName == "Mark") {
+      this.currentBookName = "Genesis"
+    } else {
+      this.currentBookName = "Mark"
     }
   }
 
-  nextPage() {
-    if(this.book_entry !== null) {
-      if(this.currentBook.title == "Bible") {
-        if(this.pageNumber == this.book_entry.chapters.length) {
-          alert('This is the last page of '+ this.currentBook.title +'!');
-        }
-        else {
-          this.pageNumber++;
-          this.displayPage();
-        }
-      }
+  previousPage() {
+    // if(this.book_entry !== null) {
+    //   if(this.currentBook.title == "Bible") {
+    //     if(this.pageNumber < 2) {
+    //       alert('This is the first page of '+ this.currentBook.title +'!');
+    //     }
+    //     else {
+    //       this.pageNumber--;
+    //       this.displayPage();
+    //     }
+    //   }
+    // }
+
+    if(this.page1["chapter"] != 1) {
+      this.page1["chapter"]--;
     }
+
+  }
+
+  nextPage() {
+    // if(this.book_entry !== null) {
+    //   if(this.currentBook.title == "Bible") {
+    //     if(this.pageNumber == this.book_entry.chapters.length) {
+    //       alert('This is the last page of '+ this.currentBook.title +'!');
+    //     }
+    //     else {
+    //       this.pageNumber++;
+    //       this.displayPage();
+    //     }
+    //   }
+    // }
+
+    this.page1["chapter"]++;
+
   }
 
   jumpToPage(pageNum) {
